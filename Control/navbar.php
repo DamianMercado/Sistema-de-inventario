b50d21bd0a2f4b568c5a8634d92a7e95867e87fb
cindia17 <216095690+cindia17@users.noreply.github.com>
2026-06-10 20:11:04 -0400

    cambio del emcabezado
    terminal

--- Control/navbar.php
+++ Control/navbar.php
@@ -12,7 +12,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Revolucionario</a>
+      <a class="navbar-brand" href="#">terminal</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
